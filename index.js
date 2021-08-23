3231c7a2e0dbcb6455af7052ed1098bca48de489
surfcurse <amrahmed4376@gmail.com>
2021-08-23 02:08:15 -0400

    y

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-
-const app = express()
-
-app.listen(3000, () => {
-    console.log('App listening at http://localhost:3000')
-})
